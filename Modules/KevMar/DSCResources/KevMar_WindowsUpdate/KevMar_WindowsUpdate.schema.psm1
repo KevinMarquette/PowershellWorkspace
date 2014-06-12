329d9c66940ba52899d6adccc53faf114809f72e
Kevin Marquette <kevmar@gmail.com>
2014-06-11 21:47:22 -0500

    Added documentation to WindowsUpdate

--- Modules/KevMar/DSCResources/KevMar_WindowsUpdate/KevMar_WindowsUpdate.schema.psm1
+++ Modules/KevMar/DSCResources/KevMar_WindowsUpdate/KevMar_WindowsUpdate.schema.psm1
@@ -1,4 +1,38 @@
-﻿
+﻿<#
+.SYNOPSIS
+Configures Automatic Updates for a system.
+
+.NOTES
+This resource manages the registry keys related to Windows Updates. Detailed documentation related to the underlying registry keys can be found here: http://technet.microsoft.com/en-us/library/dd939844(v=ws.10).aspx
+
+.PARAMETER AutomaticUpdate
+Enables or disables automatic updates
+
+.PARAMETER AutoUdateOptions
+
+"Notify" = Notify before download.
+
+"Download" = Automatically download and notify of installation. 
+
+"Install" = Automatically download and schedule installation. Only valid if values exist for ScheduledInstallDay and ScheduledInstallTime.
+
+"Configurable" = Automatic Updates is required and users can configure it.
+
+.PARAMETER UseWUServer
+Enables or disables the use of WSUS to manage updates.
+.PARAMETER WUServer
+HTTP(S) URL of the WSUS server that is used by Automatic Updates and API callers (by default). 
+
+.PARAMETER TargetGroup
+Name of the computer group to which the computer belongs. 
+
+.PARAMETER ScheduledInstallDay
+"Every day","Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"
+        
+.PARAMETER ScheduledInstallTime
+Hour of the day from 0 to 23
+
+#>
 Configuration KevMar_WindowsUpdate
 {
     param
