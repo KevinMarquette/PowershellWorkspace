b63c39fdcb290ebefcbf4062191c197cf261f6fa
Kevin Marquette <kevmar@gmail.com>
2014-06-12 14:36:23 -0500

    Added Get and Test TargetResoure functions

--- Modules/KevMar/DSCResources/KevMar_Printer/KevMar_Printer.psm1
+++ Modules/KevMar/DSCResources/KevMar_Printer/KevMar_Printer.psm1
@@ -4,11 +4,29 @@
 #######################################################################
 function Get-TargetResource
 {
+    [CmdletBinding()]
+	[OutputType([System.Collections.Hashtable])]
     param
 	(	
-        
+        # Name of printer
+        [parameter(Mandatory)]
+        [string] $Name
 	)
-	
+    
+    $printer = Get-WmiObject Win32_Printer | Where-Object{$_.Name -eq $Name}	
+
+    @{
+        Name = $printer.$Name
+        DriverName = $printer.DriverName
+        PrinterIP = $printer.PortName
+        PortName = $printer.PortName
+        isShared = $printer.Shared
+        ShareName = $printer.ShareName
+        Location = $printer.Location
+        Comment= $printer.Comment
+        DeviceID = $printer.DeviceID
+    }
+
 }
 
 ######################################################################## 
@@ -16,11 +34,60 @@ function Get-TargetResource
 ########################################################################
 function Set-TargetResource
 {
+    [CmdletBinding()]
 	param
 	(	
-        
+        # Name of printer
+        [parameter(Mandatory)]
+        [string] $Name,
+
+        # Name of driver
+        [parameter(Mandatory)]
+        [string] $DriverName,
+
+        # IP address of printer
+        [parameter(Mandatory)]
+        [string] $PrinterIP,
+
+        # Name of printer port
+        [string] $PortName,
+
+        # Enable printer sharing
+        [Boolean] $isShared=$false,
+
+        # Name of shared printer
+        [string] $ShareName,
+
+        # Location of printer
+        [string] $Location="",
+
+        # Additional comments
+        [string] $Comment="",
+
+        # Device ID
+        [string] $DeviceID
 	)
 
+
+
+    $port = ([WMICLASS]"\\localhost\ROOT\cimv2:Win32_TCPIPPrinterPort").createInstance()
+    $port.Name= $Portname
+    $port.SNMPEnabled=$false
+    $port.Protocol=1
+    $port.HostAddress= $PrinterIP
+    $port.Put()
+
+    $print = ([WMICLASS]"\\localhost\ROOT\cimv2:Win32_Printer").createInstance()
+    $print.drivername = $DriverName
+    $print.PortName = $PortName
+    if($isShared){
+        $print.Shared = $isShared
+        $print.Sharename = $ShareName
+    }
+    $print.Location = $Location
+    $print.Comment = $Comment
+    $print.DeviceID = $DeviceID
+    $print.Put()
 }
 
 #######################################################################
@@ -30,9 +97,38 @@ function Test-TargetResource
 {
 	[CmdletBinding()]
 	param
-	(
-        
+	(	
+        # Name of printer
+        [parameter(Mandatory)]
+        [string] $Name,
+
+        # Name of driver
+        [parameter(Mandatory)]
+        [string] $DriverName,
+
+        # IP address of printer
+        [parameter(Mandatory)]
+        [string] $PrinterIP,
+
+        # Name of printer port
+        [string] $PortName,
+
+        # Enable printer sharing
+        [Boolean] $isShared=$false,
+
+        # Name of shared printer
+        [string] $ShareName,
+
+        # Location of printer
+        [string] $Location="",
+
+        # Additional comments
+        [string] $Comment="",
+
+        # Device ID
+        [string] $DeviceID
 	)
+
     
 }
 #######################################################################
