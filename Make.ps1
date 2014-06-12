68a126c6c79b96189bdae47263c9ab207bfb08c2
Kevin Marquette <kevmar@gmail.com>
2014-06-11 20:23:17 -0500

    added console messages

--- Make.ps1
+++ Make.ps1
@@ -1,3 +1,3 @@
 ﻿# Deploys modules to the local system
-
+Write-Host "Copying .\Modules\KevMar to 'C:\Program Files\WindowsPowerShell\Modules\'" -ForegroundColor Yellow
 Copy-Item .\Modules\KevMar 'C:\Program Files\WindowsPowerShell\Modules\' -Recurse -Force
