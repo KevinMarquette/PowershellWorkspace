cfd5f109ef4a12c618c2bc2b4cc4ae8638d5af43
Kevin Marquette <kevmar@gmail.com>
2014-06-11 20:54:26 -0500

    polished console output

--- Make.ps1
+++ Make.ps1
@@ -1,3 +1,4 @@
 ﻿# Deploys modules to the local system
 Write-Host "Copying .\Modules\KevMar to 'C:\Program Files\WindowsPowerShell\Modules\'" -ForegroundColor Yellow
 Copy-Item .\Modules\KevMar 'C:\Program Files\WindowsPowerShell\Modules\' -Recurse -Force
+Write-Host "Done" -ForegroundColor Yellow
