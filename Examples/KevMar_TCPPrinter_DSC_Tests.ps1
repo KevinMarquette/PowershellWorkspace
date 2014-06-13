26f52e9e56a8174f5f4372a72257f047bca4ecfb
Kevin Marquette <kevmar@gmail.com>
2014-06-12 23:20:08 -0500

    Added test for TCPPrinter

---
+++ Examples/KevMar_TCPPrinter_DSC_Tests.ps1
@@ -0,0 +1,22 @@
+﻿$printer = @{
+    PortName   =  "10.112.11.113"                                                        
+    DriverName =  "EPSON NX430 Series"                                                   
+    PrinterIP  =  "10.112.11.113"                                                        
+    Comment    =  "Comment"                                                                               
+    isShared   =  $false                                                                                                                              
+    Name       =  "EPSON NX430 Series"                                                                            
+    Location   =  "Location"                                                                          
+    DeviceID   =  "EPSON NX430 Series" 
+    ShareName = "ShareName"                                                  
+}
+
+Test-TargetResource @printer -Verbose
+
+
+Set-TargetResource @printer -Verbose
+
+
+Set-TargetResource -Name "EPSON NX430 Series" -Ensure Absent -Verbose
+
+Get-WmiObject win32_printer | where name -eq $printer.Name | fl *
+
