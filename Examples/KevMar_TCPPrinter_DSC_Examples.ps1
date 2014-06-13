80bd8f160fc3a29532480fc6b5cd6b097add9844
Kevin Marquette <kevmar@gmail.com>
2014-06-13 15:50:49 -0500

    Added TCPPrinter examples

---
+++ Examples/KevMar_TCPPrinter_DSC_Examples.ps1
@@ -0,0 +1,63 @@
+﻿Configuration ConfigExample
+{
+   Import-DscResource -ModuleName KevMar
+   Node localhost
+   {
+       TCPPrinter EpsonPrinter
+       {
+            Name       =  "EPSON NX430 Series"
+            DeviceID   =  "EPSON NX430 Series"
+            DriverName =  "EPSON NX430 Series"
+            PortName   =  "10.112.11.113"
+            PrinterIP  =  "10.112.11.113"
+            Comment    =  "Comment"
+            Location   =  "Location"
+            isShared   =  $false
+            ShareName  = "TestShare"
+            Ensure     = "Present"
+       }
+   }
+}
+
+ConfigExample
+
+Start-DscConfiguration -Wait -Verbose -Path .\ConfigExample
+
+# Minimal values for config to work
+
+Configuration MinConfig
+{
+   Import-DscResource -ModuleName KevMar
+   Node localhost
+   {
+       TCPPrinter EpsonPrinter
+       {
+            Name       =  "EPSON NX430 Series"            
+            DriverName =  "EPSON NX430 Series"            
+            PrinterIP  =  "10.112.11.113"            
+       }
+   }
+}
+
+MinConfig
+
+Start-DscConfiguration -Wait -Verbose -Path .\MinConfig
+
+# Remove printer
+
+Configuration RemoveTCPPrinter
+{
+   Import-DscResource -ModuleName KevMar
+   Node localhost
+   {
+       TCPPrinter EpsonPrinter
+       {
+            Name       =  "EPSON NX430 Series"            
+            Ensure     =  "Absent"          
+       }
+   }
+}
+
+RemoveTCPPrinter
+
+Start-DscConfiguration -Wait -Verbose -Path .\RemoveTCPPrinter
