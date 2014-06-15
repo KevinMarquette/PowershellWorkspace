97b681c4cf4bf1bc0c4403917a53f17a67e36c32
Kevin Marquette <kevmar@gmail.com>
2014-06-15 16:51:55 -0500

    Make file now restarts DSC to flush resource cache

--- Make.ps1
+++ Make.ps1
@@ -1,4 +1,18 @@
 ﻿# Deploys modules to the local system
-Write-Host "Copying .\Modules\KevMar to 'C:\Program Files\WindowsPowerShell\Modules\'" -ForegroundColor DarkGreen
+Write-Host "Copying .\Modules\KevMar to 'C:\Program Files\WindowsPowerShell\Modules\'"
 Copy-Item .\Modules\KevMar 'C:\Program Files\WindowsPowerShell\Modules\' -Recurse -Force
-Write-Host "Done" -ForegroundColor DarkGreen
+
+###
+### find the Process that is hosting the DSC engine
+###
+$dscProcessID = Get-WmiObject msft_providers | 
+Where-Object {$_.provider -like 'dsccore'} | 
+Select-Object -ExpandProperty HostProcessIdentifier 
+
+###
+### Kill it
+###
+Write-Host "Restarting DSC process"
+Get-Process -Id $dscProcessID | Stop-Process
+
+Write-Host "Done"
