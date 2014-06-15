ec0be24a299ed9087d32f88c52dbb910d02a3304
Kevin Marquette <kevmar@gmail.com>
2014-06-15 13:26:52 -0500

    Added verbose details

--- Modules/KevMar/DSCResources/KevMar_MapPrinter/KevMar_MapPrinter.psm1
+++ Modules/KevMar/DSCResources/KevMar_MapPrinter/KevMar_MapPrinter.psm1
@@ -15,10 +15,12 @@ function Get-TargetResource
     
     $printer = Get-WmiObject Win32_Printer | Where-Object{$_.Name -eq $Path}	
     if($printer){
+       Write-Verbose "Printer $path found"
        return @{
             Name = $printer.Name
         }
     } else {
+        Write-Verbose "Not found"
         return $null;
     }
 }
@@ -42,11 +44,12 @@ function Set-TargetResource
 	)
 
     if($Ensure -eq "Present"){     
-        
+        Write-Verbose "Adding printer $path"
         rundll32 printui.dll,PrintUIEntry /ga /n$path /z
     } 
     else #$Ensure -eq "Absent"
     {
+        Write-Verbose "Removing printer $path"
          rundll32 printui.dll,PrintUIEntry /gd /n$path
     }
 }
@@ -71,12 +74,19 @@ function Test-TargetResource
     $printer = Get-TargetResource -Path $Path
 
     # found printer and there should be one
-    if($printer -ne $null -and $Ensure -eq "Present"){return $true}
+    if($printer -ne $null -and $Ensure -eq "Present"){
+        Write-Verbose "found printer and there should be one"
+        return $true
+    }
 
     # no printer should be found
-    elseif($printer -eq $null -and $Ensure -eq "Absent"){return $true}
+    elseif($printer -eq $null -and $Ensure -eq "Absent"){
+        Write-Verbose "no printer should be found"
+        return $true
+    }
 
     # everything else indicates test failed
+    Write-Verbose "Test for resource failed"
     return $false
     
 }
