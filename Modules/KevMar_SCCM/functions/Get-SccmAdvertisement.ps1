15783066b6be5259d81546bee222c6c56907a3b1
Kevin Marquette <Kevin.Marquette@mm-games.com>
2016-02-28 21:51:10 -0600

    removed 2 functions that never got tested

--- Modules/KevMar_SCCM/functions/Get-SccmAdvertisement.ps1
+++ Modules/KevMar_SCCM/functions/Get-SccmAdvertisement.ps1
@@ -1,13 +1,15 @@
-#List all the application advertisements and general stats
-function Get-SccmAdvertisement([string]$filter){
-    $advertisements = get-wmiobject -namespace 'root\sms\site_$site' -ComputerName $computername -query "select * from SMS_ClientAdvertisementSummary" |
-             Select-Object AdvertisementID, AdvertisementName, Succeeded, Failed, Running, Retrying, Waiting, Targeted |
-             Sort-Object AdvertisementName
-
-    if($filter){
-        $advertisements |  ?{$_.advertisementName -imatch $filter -or $_.advertisementID -imatch $filter} 
-          }else{
-        $advertisements
-    }
-}
+# i like the idea behind this but abandoned it when I didn't need it
+# List all the application advertisements and general stats
+# function Get-SccmAdvertisement([string]$filter)
+# {
+#     $advertisements = get-wmiobject -namespace 'root\sms\site_$site' -ComputerName $computername -query "select * from SMS_ClientAdvertisementSummary" |
+#              Select-Object AdvertisementID, AdvertisementName, Succeeded, Failed, Running, Retrying, Waiting, Targeted |
+#              Sort-Object AdvertisementName
+# 
+#     if($filter){
+#         $advertisements |  ?{$_.advertisementName -imatch $filter -or $_.advertisementID -imatch $filter} 
+#           }else{
+#         $advertisements
+#     }
+# }
 
