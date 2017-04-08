742a21ee423055b8a3ea5fdc409951b2b2988d86
KevinMarquette <kevmar@gmail.com>
2017-04-08 13:10:21 -0700

    added .net type query functions

---
+++ Modules/Kevmar/functions/Get-DotNetType.ps1
@@ -0,0 +1,27 @@
+﻿
+function Get-DotNetType
+{
+    param(
+        $TypeName = "*exception"
+    )
+    $thisAssembly = [System.Reflection.Assembly]::GetExecutingAssembly()
+	$assemblyList = $thisAssembly.GetReferencedAssemblies()
+    foreach($assemblyName in $assemblyList) # $assemblyName = $assemblyList | select -first 1
+    {
+        $assembly = [System.Reflection.Assembly]::Load($assemblyName)
+        $moduleList = $assembly.GetModules()
+        foreach($module in $assembly.GetModules()) # $module = $moduleList | select -first 1 -skip 1
+        {
+            $typeList = $module.GetTypes()
+            foreach($type in $typeList)
+            {
+                if($type.fullname -like $TypeName)
+                {
+                    $type
+                }
+            }
+        }
+    }
+}
+
+
---
+++ Modules/Kevmar/functions/Get-TypeDescriptions.ps1
@@ -0,0 +1,18 @@
+﻿function Get-TypeDescriptions
+{
+    [cmdletbinding()]
+    param()
+
+    $library = @{}
+    $xmlFiles = LS 'C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETPortable\v4.6\*.xml'
+    foreach($file in $xmlFiles)
+    {
+        $xml = [xml]::new()
+        $xml.Load($file)
+        $xml.doc.members.member |
+           Where Name -Like 'T:*' | 
+           ForEach-Object{$library[($_.name.replace('T:',''))] = $_.summary}
+    }
+
+    $library
+}
